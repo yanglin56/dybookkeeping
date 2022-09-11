1130b77d4d4d84f6fd82c0cb7363921813648f91
yanglin <yanglin556@yahoo.com>
2022-09-12 00:12:52 +0800

    use Vuex

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -10,8 +10,6 @@
       />
     </div>
     <Tags />
-    {{ count }}
-    <button @click="add">+1</button>
   </Layout>
 </template>
 
@@ -22,28 +20,17 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import { Component } from 'vue-property-decorator';
-import store from '@/store/index2';
+import oldStore from '@/store/index2';
 @Component({
   components: { Tags, FormItem, Types, NumberPad },
   computed: {
-    count() {
-      return store.count;
-    },
     recordList() {
-      return store.recordList;
+      return this.$store.state.count;
     },
   },
 })
 export default class Money extends Vue {
-  Layout: any;
-  NumberPad: any;
-  Types: any;
-  FormItem: any;
-  Tags: any;
-  count: any;
-  add() {
-    store.addCount();
-  }
+  recordList = oldStore.recordList;
   record: RecordItem = {
     tags: [],
     notes: '',
@@ -54,7 +41,7 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    store.createRecord(this.record);
+    oldStore.createRecord(this.record);
   }
 }
 </script>
