abebaddf51e3947563a92b2fdc8976e63cec67a7
yanglin <yanglin556@yahoo.com>
2022-09-12 00:21:07 +0800

    Money.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,10 +19,16 @@
 <script lang="ts">
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
-import store from '@/store/index2';
-@Component
+@Component({
+  computed: {
+    tagList() {
+      // TODO
+      // return this.$store.fetchTags();
+      return [];
+    },
+  },
+})
 export default class Tags extends Vue {
-  tagList = store.fetchTags();
   selectedTags: string[] = [];
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
@@ -38,7 +44,8 @@ export default class Tags extends Vue {
     if (!name) {
       return window.alert('标签名不能为空');
     }
-    store.createTag(name);
+    // TODO
+    // store.createTag(name);
   }
 }
 </script>
