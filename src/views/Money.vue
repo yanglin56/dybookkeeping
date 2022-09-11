a42b665139b584afcb38f39dc33ea8c3a55c5d4d
yanglin <yanglin556@yahoo.com>
2022-09-11 23:16:57 +0800

    将所有数据操作封装在 store 里

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -19,15 +19,14 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
-import { Component, Watch } from 'vue-property-decorator';
-import recordListModel from '@/models/recordListModel';
-const recordList = recordListModel.fetch();
+import { Component } from 'vue-property-decorator';
+import store from '@/store/index2';
 @Component({
   components: { Tags, FormItem, Types, NumberPad },
 })
 export default class Money extends Vue {
-  tags = window.tagList;
-  recordList: RecordItem[] = recordList;
+  tags = store.tagList;
+  recordList = store.recordList;
   record: RecordItem = {
     tags: [],
     notes: '',
@@ -41,11 +40,7 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    recordListModel.create(this.record);
-  }
-  @Watch('recordList')
-  onRecordListChange() {
-    recordListModel.save();
+    store.createRecord(this.record);
   }
 }
 </script>
