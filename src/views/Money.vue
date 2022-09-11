e96dbbb7e184549c09908ef50bfbc73ffb517825
yanglin <yanglin556@yahoo.com>
2022-09-11 22:47:44 +0800

    封装 tagList 的增删改查

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -21,7 +21,6 @@ import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import { Component, Watch } from 'vue-property-decorator';
 import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
 const recordList = recordListModel.fetch();
 @Component({
   components: { Tags, FormItem, Types, NumberPad },
