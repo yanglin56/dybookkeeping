4ab2190424594bbfc5858dbc4d67d2a4de8ebc49
yanglin <yanglin556@yahoo.com>
2022-09-11 21:57:45 +0800

    重新封装 recordListModel

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -6,7 +6,7 @@
         type="text"
         :value="value"
         @input="onValueChanged($event.target.value)"
-        :placeholder="this.placeholder"
+        :placeholder="placeholder"
       />
     </label>
   </div>
@@ -20,7 +20,7 @@ export default class FormItem extends Vue {
   @Prop({ default: '' }) readonly value!: string;
   @Prop({ required: true }) fieldName!: string;
   @Prop() placeholder?: string;
-  @Watch('value')
+
   onValueChanged(value: string) {
     this.$emit('update:value', value);
   }
