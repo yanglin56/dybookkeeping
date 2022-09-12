a641a951d70e03025a92ecdea0ee11425fad5c12
yanglin <yanglin556@yahoo.com>
2022-09-12 21:16:00 +0800

    添加 height 示例

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,51 +1,45 @@
 <template>
   <Layout>
-    <Tabs
-      class-prefix="type"
-      :data-source="recordTypeList"
-      :value.sync="type"
-    />
-    <Tabs
-      class-prefix="interval"
-      :data-source="intervalList"
-      :value.sync="interval"
-    />
+    <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
+    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     <div>
-      type: {{ type }}
-      <br />
-      interval: {{ interval }}
+      type: {{type}}
+      <br/>
+      interval: {{interval}}
     </div>
   </Layout>
 </template>
 
 <style scoped lang="scss">
-::v-deep .type-tabs-item {
-  background: white;
-  &.selected {
-    background: #c4c4c4;
-    &::after {
-      display: none;
+  ::v-deep {
+    .type-tabs-item {
+      background: white;
+      &.selected {
+        background: #C4C4C4;
+        &::after {
+          display: none;
+        }
+      }
+    }
+    .interval-tabs-item {
+      /*height: 48px;*/
     }
   }
-}
-::v-deep .interval-tabs-item {
-  height: 48px;
-}
 </style>
 
 <script lang="ts">
-import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
-import Tabs from '@/components/Tabs.vue';
-import intervalList from '@/constants/intervalList';
-import recordTypeList from '@/constants/recordTypeList';
-@Component({
-  components: { Tabs },
-})
-export default class Statistics extends Vue {
-  type = '-';
-  interval = 'day';
-  intervalList = intervalList;
-  recordTypeList = recordTypeList;
-}
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+  import Tabs from '@/components/Tabs.vue';
+  import intervalList from '@/constants/intervalList';
+  import recordTypeList from '@/constants/recordTypeList';
+  @Component({
+    components: {Tabs},
+  })
+  export default class Statistics extends Vue {
+    type = '-';
+    interval = 'day';
+    intervalList = intervalList;
+    recordTypeList = recordTypeList;
+  }
 </script>
