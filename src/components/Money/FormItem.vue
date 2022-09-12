c7278ded12893be4428a978e571e4baa7ddfbe27
yanglin <yanglin556@yahoo.com>
2022-09-12 18:18:10 +0800

    修复刷新变成 404 的 bug

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -20,7 +20,6 @@ export default class FormItem extends Vue {
   @Prop({ default: '' }) readonly value!: string;
   @Prop({ required: true }) fieldName!: string;
   @Prop() placeholder?: string;
-
   onValueChanged(value: string) {
     this.$emit('update:value', value);
   }
