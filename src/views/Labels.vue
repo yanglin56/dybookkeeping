5d58d950374138a74efbb3ee9a2baf5b3c519585
yanglin <yanglin556@yahoo.com>
2022-09-12 14:20:45 +0800

    所有 computed 改成 get 函数

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -25,13 +25,11 @@ import { mixins } from 'vue-class-component';
 import TagHelper from '@/mixins/TagHelper';
 @Component({
   components: { Button },
-  computed: {
-    tags() {
-      return this.$store.state.tagList;
-    },
-  },
 })
 export default class Labels extends mixins(TagHelper) {
+  get tags() {
+    return this.$store.state.tagList;
+  }
   beforeCreate() {
     this.$store.commit('fetchTags');
   }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -22,13 +22,11 @@ import Tags from '@/components/Money/Tags.vue';
 import { Component } from 'vue-property-decorator';
 @Component({
   components: { Tags, FormItem, Types, NumberPad },
-  computed: {
-    recordList() {
-      return this.$store.state.recordList;
-    },
-  },
 })
 export default class Money extends Vue {
+  get recordList() {
+    return this.$store.state.recordList;
+  }
   record: RecordItem = {
     tags: [],
     notes: '',
