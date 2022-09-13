8518e929baa96863612046a1b67a1d53436598e0
yanglin <yanglin556@yahoo.com>
2022-09-13 23:06:39 +0800

    添加 createdAt输入框

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -2,17 +2,28 @@
   <div>
     <label class="formItem">
       <span class="name">{{ this.fieldName }}</span>
-      <input
-        type="text"
-        :value="value"
-        @input="onValueChanged($event.target.value)"
-        :placeholder="placeholder"
-      />
+      <template v-if="type === 'date'">
+        <input
+          :type="type || 'text'"
+          :value="x(value)"
+          @input="onValueChanged($event.target.value)"
+          :placeholder="placeholder"
+        />
+      </template>
+      <template v-else>
+        <input
+          :type="type || 'text'"
+          :value="value"
+          @input="onValueChanged($event.target.value)"
+          :placeholder="placeholder"
+        />
+      </template>
     </label>
   </div>
 </template>
 
 <script lang="ts">
+import dayjs from 'dayjs';
 import Vue from 'vue';
 import { Component, Prop, Watch } from 'vue-property-decorator';
 @Component
@@ -20,8 +31,14 @@ export default class FormItem extends Vue {
   @Prop({ default: '' }) readonly value!: string;
   @Prop({ required: true }) fieldName!: string;
   @Prop() placeholder?: string;
+  @Prop() type?: string;
+
   onValueChanged(value: string) {
+    console.log(value);
     this.$emit('update:value', value);
+  }
+  x(isoString: string) {
+    return dayjs(isoString).format('YYYY-MM-DD');
   }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,6 +2,14 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
     <Tabs :data-source="recordTypeList" :value.sync="record.type" />
+    <div class="createdAt">
+      <FormItem
+        field-name="日期"
+        type="date"
+        placeholder="在这里输入日期"
+        :value.sync="record.createdAt"
+      />
+    </div>
     <div class="notes">
       <FormItem
         field-name="备注"
@@ -34,6 +42,7 @@ export default class Money extends Vue {
     notes: '',
     type: '-',
     amount: 0,
+    createdAt: new Date().toISOString(),
   };
   created() {
     this.$store.commit('fetchRecords');
