6e53fef40c7e82bc87050fc1a2fcb0549b1b7550
yanglin <yanglin556@yahoo.com>
2022-09-18 23:49:18 +0800

    基本完成 chart

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -100,6 +100,11 @@ export default class Statistics extends Vue {
         data: keys,
         axisTick: { alignWithLabel: true },
         axisLine: { lineStyle: { color: '#666' } },
+        axisLabel: {
+          formatter: function (value: string, index: number) {
+            return value.substr(5);
+          },
+        },
       },
       yAxis: {
         type: 'value',
